9790272b6567c4888768d84ceb9fb43a4a7298c6
Ezgi Bas <52749796+ezgibas@users.noreply.github.com>
2023-04-05 16:48:30 -0400

    done

--- src/App.js
+++ src/App.js
@@ -3,59 +3,86 @@ import './App.css';
 import './button.css';
 import Button from '@mui/material/Button';
 import React, {useState, useEffect} from 'react';
+import { display } from '@mui/system';
 
 function App() {
+  useEffect(() => {
+    document.title = 'QTC Helper';
+  }, []);
 
-  const [display, setDisplay] = useState(<div></div>)
   const [displayData, setDisplayData] = useState({})
+  const [reveal, setReveal] = useState("")
+  const [routine, setRoutine] = useState("vibe check")
 
-  // TODO: USE THIS INSTEAD OF DISPLAYDATA
-  function Display({setup, delivery}) {
-    useEffect(() => {
-
-    })
+  function Display(routine) {
+    if(routine !== "vibe check") {
+      return(
+        <div class="contentDiv" id="text">
+          <p>{displayData["setup"]}</p>
+          <p>{reveal}</p>
+          <div class = "buttonHolder">
+            <Button class="button" id="revealButton" type="button" onClick={handleRevealButton}>Reveal</Button>
+          </div>
+        </div>
+      )
+    }
+    else {
+      return(
+        <div class="contentDiv" id="text">
+          <p>{displayData["setup"]}</p>
+        </div>
+      )
+    }
   }
 
   const handleRevealButton = (e) => {
     e.preventDefault()
+    setReveal(displayData.delivery)
     console.log("reveal")
-    setDisplay(
-      <div class="contentDiv" id="text">
-       <p>{displayData["setup"]}</p>
-       <p>{displayData["reveal"]}</p>
-      </div>
-    )
   }
 
   const HandleJoke = (e) => {
-    useEffect(() => {
-      fetch("https://v2.jokeapi.dev/joke/Any?type=twopart&safe-mode")
+    setRoutine("joke")
+    setReveal("")
+    fetch("https://v2.jokeapi.dev/joke/Any?type=twopart&safe-mode")
         .then(response => response.json())
         .then(data => {
           setDisplayData({
-            setup: data.setup,
-            reveal: data.delivery
+            'setup': data.setup,
+            'delivery': data.delivery
           });
         })
-      }
-    )
-      setDisplay(
-        <div class="contentDiv" id="text">
-         <p>{displayData["setup"]}</p>
-         <Button type="button" onClick={handleRevealButton}>Reveal Punchline</Button>
-        </div>
-      )
-    console.log(displayData["setup"] + " " + displayData["reveal"])
   };
+
+  
 
   const handleRiddleButton = (e) => {
     e.preventDefault()
-    //setText("hello this is a riddle")
+    setRoutine("riddle")
+    setReveal("")
+    fetch("https://riddles-api.vercel.app/random")
+        .then(response => response.json())
+        .then(data => {
+          setDisplayData({
+            'setup': data.riddle,
+            'delivery': data.answer
+          });
+        })
   };
+
+  const vibeChecks = ["Put an emoji in the chat describing your mood!", "Describe your mood with a green emoji!", 
+  "What are you excited about in the next week/month?", "Do a rose/bud/thorn for your week/weekend!", 
+  "What was your favorite thing from the last week?", "Describe your mood in 3 words!", "What's the best movie/show you've seen recently?",
+  "What's the best book you've read recently?", "Describe your mood with a song!"]
 
   const handleVibeCheckButton = (e) => {
     e.preventDefault()
-    //setText("hello this is a vibe check")
+    setRoutine("vibe check")
+    setReveal("")
+    const check = vibeChecks[Math.floor(Math.random() * vibeChecks.length)]
+    setDisplayData({
+      'setup': check,
+    })
   };
 
   
@@ -64,13 +91,13 @@ function App() {
       <header >
       </header>
       <div class="contentDiv" id="mainContent"> 
-      <h1>Good Morning QTC!</h1>
+          <h1>Good Morning QTC!</h1>
         <div class="buttonHolder">
           <Button class = "button" type="button" onClick={HandleJoke}>Get a Joke</Button>
           <Button class = "button" type="button" onClick={handleRiddleButton}>Get a Riddle</Button>
           <Button class = "button" type="button" onClick={handleVibeCheckButton}>Get a Vibe Check</Button>
         </div>
-        {display}
+        {Display(routine)}
       </div>
     </div>
   );
